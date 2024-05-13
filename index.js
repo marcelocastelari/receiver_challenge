b2a626b07dc6bf30a8144fa6cece17f1d3faf090
marcelocastelari <marcelocastelari@gmail.com>
2024-05-12 23:51:18 -0300

    feat: add data.json and test dependencies

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const pino = require('pino');
 const server = require('./src/main/server')
 const { PORT } = require('./config');
 const dbConnect = require('./db/config');
+const { checkAndExecute } = require('./src/main/utils/createInitialFilesUtil')
+const receivers = require('./db/data.json')
 
 const logger = pino();
 
 server.listen(PORT, () => {
     logger.info(`Server is running on port ${PORT}`);
     dbConnect();
+    checkAndExecute(receivers);
 });
 
 module.exports = server;
